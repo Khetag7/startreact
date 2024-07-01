26506c7ad8248a4d23e059329289ee4fe7602d53
Khetag K <162562952+Khetag7@users.noreply.github.com>
2024-07-01 11:19:50 +0300

    New Commit From React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
+import { Header } from './layout/Header.'
+import { Footer } from './layout/Footer.'
+import { Main } from './layout/Main.'
 
 function App() {
-  return (
-    <div className="App">
-      React Start
-    </div>
-  );
+	return (
+		<>
+			<Header />
+			<Main />
+			<Footer />
+		</>
+	)
 }
 
-export default App;
+export default App
