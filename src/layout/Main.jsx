30b171fa14eb074aa352c1f4cf69d7079f698c90
Khetag K <162562952+Khetag7@users.noreply.github.com>
2024-07-31 11:52:41 +0300

    project complete

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Movies } from '../components/Movies';
 import { Search } from '../components/Search';
 import { Preloader } from '../components/Preloader';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends React.Component {
 	state = {
 		movies: [],
@@ -9,7 +12,8 @@ class Main extends React.Component {
 	};
 
 	componentDidMount() {
-		fetch('http://www.omdbapi.com/?apikey=d9c7c176&s=Spider Man')
+		console.log(process.env);
+		fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=Spider Man`)
 			.then(response => response.json())
 			.then(data =>
 				this.setState({ movies: data.Search, loading: false }),
@@ -18,7 +22,7 @@ class Main extends React.Component {
 	searchMovies = (str, type = 'all') => {
 		this.setState({ loading: true });
 		fetch(
-			`http://www.omdbapi.com/?apikey=d9c7c176&s=${str}${
+			`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
 				type !== 'all' ? `&type=${type}` : ''
 			}`,
 		)
